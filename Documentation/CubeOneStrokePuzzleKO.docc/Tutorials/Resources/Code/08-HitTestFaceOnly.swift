c445e05756d4b0a5475d81e16302b54c8773a697
mongshushu <minopo53@gmail.com>
2026-08-26 23:46:59 +0900

    Polish Korean DocC tutorial notes

--- Documentation/CubeOneStrokePuzzleKO.docc/Tutorials/Resources/Code/08-HitTestFaceOnly.swift
+++ Documentation/CubeOneStrokePuzzleKO.docc/Tutorials/Resources/Code/08-HitTestFaceOnly.swift
@@ -1,20 +1,74 @@
-func handleTap(at point: CGPoint, in sceneView: SCNView) {
-    let hitResults = sceneView.hitTest(
-        point,
-        options: [
-            SCNHitTestOption.firstFoundOnly: true
+import Combine
+import SceneKit
+import SpriteKit
+import UIKit
+
+@MainActor
+final class SceneKitPuzzleViewModel: ObservableObject {
+    let scene = SCNScene()
+
+    @Published private(set) var debugText = ""
+
+    private let cameraNode = SCNNode()
+    private let cubeNode = SCNNode()
+    private let faceScene = GameScene(size: CGSize(width: 700, height: 900))
+
+    init() {
+        configureScene()
+    }
+
+    private func configureScene() {
+        scene.background.contents = UIColor.systemBackground
+
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 5)
+        scene.rootNode.addChildNode(cameraNode)
+
+        cubeNode.name = "interactiveCube"
+        cubeNode.geometry = makeCubeGeometry()
+        scene.rootNode.addChildNode(cubeNode)
+    }
+
+    private func makeCubeGeometry() -> SCNBox {
+        let box = SCNBox(width: 2.3, height: 2.3, length: 2.3, chamferRadius: 0)
+
+        let puzzleMaterial = SCNMaterial()
+        puzzleMaterial.diffuse.contents = faceScene
+        puzzleMaterial.lightingModel = .constant
+
+        let plainMaterial = SCNMaterial()
+        plainMaterial.diffuse.contents = UIColor.systemGray5
+
+        box.materials = [
+            puzzleMaterial,
+            plainMaterial,
+            plainMaterial,
+            plainMaterial,
+            plainMaterial,
+            plainMaterial
         ]
-    )
 
-    guard let hitResult = hitResults.first else {
-        debugText = "큐브를 터치하지 않았어요."
-        return
+        return box
     }
 
-    if hitResult.node !== cubeNode {
-        debugText = "퍼즐 큐브가 아닌 곳을 터치했어요."
-        return
-    }
+    func handleTap(at point: CGPoint, in sceneView: SCNView) {
+        let hitResults = sceneView.hitTest(
+            point,
+            options: [
+                SCNHitTestOption.firstFoundOnly: true
+            ]
+        )
 
-    debugText = "터치한 cube material index: \(hitResult.geometryIndex)"
+        guard let hitResult = hitResults.first else {
+            debugText = "큐브를 터치하지 않았어요."
+            return
+        }
+
+        if hitResult.node !== cubeNode {
+            debugText = "퍼즐 큐브가 아닌 곳을 터치했어요."
+            return
+        }
+
+        debugText = "터치한 cube material index: \(hitResult.geometryIndex)"
+    }
 }
