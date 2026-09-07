803193cf790d394f8cb010185b1760f0f478e079
mongshushu <minopo53@gmail.com>
2026-09-07 13:01:06 +0900

    Update Korean SpriteKit and SceneKit tutorials

---
+++ Documentation/CubeOneStrokePuzzleKO.docc/Tutorials/Resources/Code/09-TileIDFromTextureCoordinate.swift
@@ -0,0 +1,97 @@
+import Combine
+import SceneKit
+import SpriteKit
+import UIKit
+
+@MainActor
+final class SceneKitPuzzleViewModel: ObservableObject {
+    let scene = SCNScene()
+
+    @Published private(set) var debugText = ""
+
+    private let cameraNode = SCNNode()
+    private let cubeNode = SCNNode()
+    private let faceScene = GameScene(size: CGSize(width: 700, height: 900))
+
+    init() {
+        faceScene.setUpPuzzleScene()
+        configureScene()
+    }
+
+    private func configureScene() {
+        scene.background.contents = UIColor.systemBackground
+
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 5)
+        scene.rootNode.addChildNode(cameraNode)
+
+        cubeNode.name = "interactiveCube"
+        cubeNode.geometry = makeCubeGeometry()
+        scene.rootNode.addChildNode(cubeNode)
+    }
+
+    private func makeCubeGeometry() -> SCNBox {
+        let box = SCNBox(width: 2.3, height: 2.3, length: 2.3, chamferRadius: 0)
+
+        let puzzleMaterial = SCNMaterial()
+        puzzleMaterial.diffuse.contents = faceScene
+        puzzleMaterial.lightingModel = .constant
+
+        let plainMaterial = SCNMaterial()
+        plainMaterial.diffuse.contents = UIColor.systemGray5
+
+        box.materials = [
+            plainMaterial,
+            plainMaterial,
+            puzzleMaterial,
+            plainMaterial,
+            plainMaterial,
+            plainMaterial
+        ]
+
+        return box
+    }
+
+    func handleTap(at point: CGPoint, in sceneView: SCNView) {
+        let hitResults = sceneView.hitTest(
+            point,
+            options: [
+                SCNHitTestOption.firstFoundOnly: true
+            ]
+        )
+
+        guard let hitResult = hitResults.first else {
+            debugText = "큐브를 터치하지 않았어요."
+            return
+        }
+
+        if hitResult.node !== cubeNode {
+            debugText = "퍼즐 큐브가 아닌 곳을 터치했어요."
+            return
+        }
+
+        let textureCoordinate = hitResult.textureCoordinates(withMappingChannel: 0)
+        let boardPosition = boardPosition(from: textureCoordinate)
+        let tileID = tileID(row: boardPosition.row, column: boardPosition.column)
+
+        debugText = "터치한 tile: \(tileID)"
+    }
+
+    private func boardPosition(from textureCoordinate: CGPoint) -> (row: Int, column: Int) {
+        let boardSize = 2
+        let column = min(
+            max(Int(textureCoordinate.x * CGFloat(boardSize)), 0),
+            boardSize - 1
+        )
+        let row = min(
+            max(Int(textureCoordinate.y * CGFloat(boardSize)), 0),
+            boardSize - 1
+        )
+
+        return (row, column)
+    }
+
+    private func tileID(row: Int, column: Int) -> String {
+        "tile_\(row)_\(column)"
+    }
+}
